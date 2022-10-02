7c64d794ccd4517e37631e4f61dde5c68d1e24ff
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-10-01 18:43:52 -0600

    hungerGames updated to d4a

---
+++ hungerGames.js
@@ -0,0 +1,29 @@
+//input variables and player name here. Have them roll before the game begins
+let name = "Simmons"
+let d20a = "9"
+let d4a = "2"
+
+console.log(`Welcome, ${name} to the 256 annual Hunger Games!`)
+console.log('It is nearly time for the gong to ring, roll the die to see how many seconds you have to prepare')
+
+if(d20a <= 5){
+    console.log(`OH NO! It looks like you only have ${d20a} seconds left.`)
+} else if(d20a > 5 && d20a <= 10){
+    console.log(`The odds are NEVER in your favor! You have ${d20a} seconds left.`)
+} else if(d20a > 10 && d20a <= 15){
+    console.log(`${name} it appears you have some fortune! yoou have ${d20a} seconds to prepare`)
+} else{
+    console.log(`Holy Mackrel! Keep an eye on this one folks! They have ${d20a} seconds to prepare!`)
+}
+
+console.log('The gong has rung! Where do you run?')
+
+if(d4a == 1){
+    console.log('You rolled a 1 and ran to the right!')
+} else if(d4a == 2){
+    console.log('You rolled a 2 and run to the left!')
+} else if(d4a == 3){
+    console.log('You have rolled a 3 and retreat behind you, into the woods!')
+} else if(d4a == 4){
+    console.log('You rolled a 4 and after hesitating a moment you run straight for the Cornicopia')
+}
